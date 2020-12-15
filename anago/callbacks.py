48b96de6fed6a12253354d8245ad773d3f363a48
flash <46099269+vamshinr@users.noreply.github.com>
2020-12-15 22:00:35 +0530

    Update callbacks.py
    added earlystopping method with callbacks and classification report and f1 score for both validation data and train data for each epoch

--- anago/callbacks.py
+++ anago/callbacks.py
@@ -5,12 +5,12 @@ import numpy as np
 from keras.callbacks import Callback
 from seqeval.metrics import f1_score, classification_report
 
-
 class F1score(Callback):
 
-    def __init__(self, seq, preprocessor=None):
+    def __init__(self, seq, train_seq, preprocessor=None):
         super(F1score, self).__init__()
         self.seq = seq
+        self.train_seq = train_seq
         self.p = preprocessor
 
     def get_lengths(self, y_true):
@@ -38,7 +38,24 @@ class F1score(Callback):
             label_true.extend(y_true)
             label_pred.extend(y_pred)
 
-        score = f1_score(label_true, label_pred)
-        print(' - f1: {:04.2f}'.format(score * 100))
-        print(classification_report(label_true, label_pred))
-        logs['f1'] = score
+        valid_score = f1_score(label_true, label_pred)
+        print(' - f1-valid: {:04.2f}'.format(valid_score * 100))
+        print('validation report :', classification_report(label_true, label_pred))
+
+        label_true = []
+        label_pred = []
+        for i in range(len(self.train_seq)):
+            x_true, y_true = self.train_seq[i]
+            lengths = self.get_lengths(y_true)
+            y_pred = self.model.predict_on_batch(x_true)
+
+            y_true = self.p.inverse_transform(y_true, lengths)
+            y_pred = self.p.inverse_transform(y_pred, lengths)
+
+            label_true.extend(y_true)
+            label_pred.extend(y_pred)
+
+        train_score = f1_score(label_true, label_pred)
+        print(' - f1-train: {:04.2f}'.format(train_score * 100))
+        print('train report :', classification_report(label_true, label_pred))
+        logs['f1'] = {"epoch":epoch,"dev_score":valid_score,"train_score":train_score}
